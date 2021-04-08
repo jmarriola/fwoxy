c31cf66cb93bb1adbbd20f2f5d70e5e7a782ad8a
Jill Arriola <jillbert79@gmail.com>
2021-04-08 15:35:15 -0400

    Explicitly draw plot

--- R/fwoxy.R
+++ R/fwoxy.R
@@ -151,25 +151,25 @@ write.csv(results, paste0(run_name, '.csv'))
 ## Plots
 
 # Plot of oxygen concentration time series
-print(ggplot(results, aes(x = t, y = c)) +
+ggplot(results, aes(x = t, y = c)) +
   geom_line(colour = "blue") +
   labs(x = "Hour of day", y = "oxy, mmol/m3") +
   theme_bw() +
   scale_x_continuous(breaks = seq(1,518400,by=43200), labels = c('1'='0','43201'='12','86401'='0','129601'='12','172801'='0','216001'='12','259201'='0','302401'='12','345601'='0','388801'='12','432001'='0','475201'='12'))
-)
 
 
 # A way to plot fluxes, includes legend outside on right
 colors <- c(gasexd = "red3", gppd = "orange", erd = "purple4", dcdtd = "steelblue3")
 fluxes <- data.frame(t, gasexd, gppd, erd, dcdtd)
 resultsNew <- fluxes %>% pivot_longer(cols = gasexd:dcdtd, names_to = 'Variables', values_to = "Value")
-print(ggplot(resultsNew, aes(x = t, y = Value, group = Variables, color = Variables)) +
+fluxPlot <- ggplot(resultsNew, aes(x = t, y = Value, group = Variables, color = Variables)) +
   theme_bw() +
   geom_line() +
   labs(x = "Hour of day", y = "Flux, mmol/m3/day") +
   scale_color_manual(values = colors) +
   scale_x_continuous(breaks = seq(1,518400,by=43200), labels = c('1'='0','43201'='12','86401'='0','129601'='12','172801'='0','216001'='12','259201'='0','302401'='12','345601'='0','388801'='12','432001'='0','475201'='12'))
-)
+
+print(fluxPlot, newpage = is.null(vp))
 }
 
 
