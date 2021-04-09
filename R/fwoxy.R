4e82a168fe6cf2b372b8223ec7fa009507a7c78a
Jill Arriola <69649635+jmarriola@users.noreply.github.com>
2021-04-09 14:32:10 -0400

    Update fwoxy.R

--- R/fwoxy.R
+++ R/fwoxy.R
@@ -162,14 +162,14 @@ colors <- c(gasexd = "red3", gppd = "orange", erd = "purple4", dcdtd = "steelblu
 fluxes <- data.frame(t, gasexd, gppd, erd, dcdtd)
 resultsNew <- fluxes %>% pivot_longer(cols = gasexd:dcdtd, names_to = 'Variables', values_to = "Value")
 
-#par(mfrow = c(2,1))
+par(mfrow = c(2,1))
 ggplot(results, aes(x = t, y = c)) +
   geom_line(colour = "blue") +
   labs(x = "Hour of day", y = "oxy, mmol/m3") +
   theme_bw() +
   scale_x_continuous(breaks = breaks, labels = labels)
 
-#print(oxyPlot)
+print(oxyPlot)
 
 ggplot(resultsNew, aes(x = t, y = Value, group = Variables, color = Variables)) +
   theme_bw() +
@@ -178,7 +178,7 @@ ggplot(resultsNew, aes(x = t, y = Value, group = Variables, color = Variables)) 
   scale_color_manual(values = colors) +
   scale_x_continuous(breaks = breaks, labels = labels)
 
-#print(fluxPlot)
+print(fluxPlot)
 
 
 # Return results data frame to the global environment
