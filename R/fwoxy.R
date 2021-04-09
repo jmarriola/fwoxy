52f832e88f9341106fe442570090cccfd4ead7c9
Jill Arriola <69649635+jmarriola@users.noreply.github.com>
2021-04-09 11:20:43 -0400

    Remove auto producing a .csv file

--- R/fwoxy.R
+++ R/fwoxy.R
@@ -146,7 +146,7 @@ varb <- c('time step', 'oxy, mmol/m3', 'troc, mmol/m3/d', 'gasex, mmol/m3/d', 'g
           'er, mmol/m3/d','oxysu, mmol/m3', 'wspd2, m2/s2', 'sc', 'kw, m/s')
 results <- data.frame(t, c, dcdtd, gasexd, gppd, erd, oxysu, wspd2, sc, kw)
 colnames(results) <- varb
-write.csv(results, paste0(run_name, '.csv'))
+# write.csv(results, paste0(run_name, '.csv'))
 
 ## Plots
 
@@ -175,6 +175,8 @@ fluxPlot <- ggplot(resultsNew, aes(x = t, y = Value, group = Variables, color = 
   scale_x_continuous(breaks = breaks, labels = labels)
 
 print(fluxPlot)
+  
+return(results)
 }
 
 
